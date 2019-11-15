0e72ab18d63e81afae1beaa71c59c163e0e2eb2f
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2019-11-15 09:05:41 -0500

    Added Line Customization Basics

--- App Dev Projects/PainterPS/PainterPS/ViewController.swift
+++ App Dev Projects/PainterPS/PainterPS/ViewController.swift
@@ -16,7 +16,7 @@ class Canvas: UIView {
         guard let context = UIGraphicsGetCurrentContext() else{
             return
         }
-        //i=index, p=point in line
+        //i=index, p=point in line, sets up the line
         for (i,p) in line.enumerated(){
             if i == 0{
                 context.move(to: p)
@@ -24,6 +24,10 @@ class Canvas: UIView {
                 context.addLine(to: p)
             }
         }
+        //sets up line's aesthetic
+        context.setLineWidth(CGFloat(20))
+        context.setStrokeColor(UIColor.red.cgColor)
+        //draws any line with current settings
         context.strokePath()
     }
     //Finger tracking function
@@ -32,22 +36,31 @@ class Canvas: UIView {
         guard let point = touches.first?.location(in: nil) else {
             return
         }
-        //print(point) <- print location tapped
         line.append(point)
-        //redraws canvas
+        //redraws canvas after tracking point
         setNeedsDisplay()
     }
 }
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var undoBtn: UIButton!
+    @IBOutlet weak var clearBtn: UIButton!
+    @IBOutlet weak var sizeSlider: UISlider!
+    @IBOutlet weak var canvasView: UIView!
     let canvas = Canvas()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //sets up canvas
         view.addSubview(canvas)
-        canvas.frame = view.frame
+        canvas.frame = canvasView.frame
         canvas.backgroundColor = .white
     }
-
-
+    //undo button clicked
+    @IBAction func undoClicked(_ sender: Any) {
+    }
+    //clear button clicked
+    @IBAction func clearClicked(_ sender: Any) {
+    }
 }
 
