4f07e9a9f7b31b5e3a207c886eae820fc2b444d5
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2019-11-18 10:29:25 -0500

    Connected Sprite to Code

--- App Dev Projects/PongPS/PongPS/GameScene.swift
+++ App Dev Projects/PongPS/PongPS/GameScene.swift
@@ -10,11 +10,22 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    //sets up variables for each item
+    var ball = SKSpriteNode()
+    var playerPaddle = SKSpriteNode()
+    var enemyPaddle = SKSpriteNode()
+    var playerScore = SKLabelNode()
+    var enemyScore = SKLabelNode()
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
+        //connects gamescene to code
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        playerPaddle = self.childNode(withName: "playerPaddle") as! SKSpriteNode
+        enemyPaddle = self.childNode(withName: "enemyPaddle") as! SKSpriteNode
+        playerScore = self.childNode(withName: "playerScore") as! SKLabelNode
+        enemyScore = self.childNode(withName: "enemyScore") as! SKLabelNode
     }
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
