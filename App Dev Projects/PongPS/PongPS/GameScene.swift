06f6c745a6780bcc20eee7100c97e5d56698f4fd
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2019-11-18 10:44:37 -0500

    Made a basic version of pong (no working scores)

--- App Dev Projects/PongPS/PongPS/GameScene.swift
+++ App Dev Projects/PongPS/PongPS/GameScene.swift
@@ -44,7 +44,25 @@ class GameScene: SKScene {
         border.restitution = 1
         self.physicsBody = border
     }
+    //finger touched screen
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            //gets finger location and moves to it with a small delay
+            let location = touch.location(in: self)
+            playerPaddle.run(SKAction.moveTo(x: location.x, duration: 0.1))
+        }
+    }
+    //finger moves
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            //gets finger location and moves to it with a small delay
+            let location = touch.location(in: self)
+            playerPaddle.run(SKAction.moveTo(x: location.x, duration: 0.1))
+        }
+    }
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        //moves the enemy, changes the duration to change the difficulty
+        enemyPaddle.run(SKAction.moveTo(x: ball.position.x, duration: 1))
     }
 }
