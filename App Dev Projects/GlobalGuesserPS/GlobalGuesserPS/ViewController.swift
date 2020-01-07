8c4c95b1c0e36249307d5135f34980cab226dc90
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2020-01-07 08:18:47 -0500

    Added Global Guesser Project

---
+++ App Dev Projects/GlobalGuesserPS/GlobalGuesserPS/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GlobalGuesserPS
+//
+//  Created by Tristan Pudell-Spatscheck on 1/7/20.
+//  Copyright © 2020 Tristan Pudell-Spatscheck. All rights reserved.
+//
+/* LOCATIONS
+ 1.)40.7865206,-111.7167087
+ 2.)68.7928111,-93.4407461
+ 3.)57.998549,102.6518604
+ 4.)-55.065834,110.0088882
+ 5.)-69.0089998,39.5768748
+ 6.)-34.9214678,-57.9552406
+ 7.)-41.445569,147.1199944
+ 8.)35.5375206,46.1419802
+ 9.)41.7440516,-91.5599823
+ 10.)-17.8258324,31.0033495
+ */
+import UIKit
+import MapKit
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
