c1bdae83c3e9851b1bdeb9b514f65bda3a8d0cb6
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2020-01-21 09:15:41 -0500

    Added PolyLine drawing to map

--- App Dev Projects/RouteMapperPS/RouteMapperPS/ViewController.swift
+++ App Dev Projects/RouteMapperPS/RouteMapperPS/ViewController.swift
@@ -11,11 +11,7 @@ NOTES/BUGS:
  */
 import UIKit
 import MapKit
-struct Location{
-    var image: UIImage
-    var coordinates: CLLocationCoordinate2D
-}
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
     //MARK - Outlets
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var resetButton: UIButton!
@@ -24,11 +20,13 @@ class ViewController: UIViewController {
         return false
     }
     var phase = 0 //phase 0 = start, 1 = end, 2 = viewing route
+    var locations: [CLLocationCoordinate2D] = []
     //MARK - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         UIDevice.current.setValue(UIInterfaceOrientation.landscapeLeft.rawValue, forKey: "orientation") //Locks screen into landscape mode
+        self.mapView.delegate = self //sets up MKMapViewDelegate
     }
     //makes a basic alert with an ok button and presents it
     func makeAlert(message: String){
@@ -53,6 +51,24 @@ class ViewController: UIViewController {
             mapView.removeAnnotation(annotation)
         }
     }
+    //creates a polyline with the given coords
+    func makePolyline(locations: [CLLocationCoordinate2D]){
+        let polyline = MKPolyline(coordinates: locations, count: locations.count) //makes polyLine w/ coords given
+        mapView.addOverlay(polyline)
+        print("polyline attempted, size: \(locations.count), coords: \(locations)")
+    }
+    //actually makes the polyline render
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        //sets rendering properties for polyline
+        if(overlay is MKPolyline){
+            print("polyline draw attempted")
+            let polylineRender = MKPolylineRenderer(overlay: overlay)
+            polylineRender.strokeColor = UIColor(ciColor: .red)
+            polylineRender.lineWidth = 5
+            return polylineRender
+        }
+        return MKPolylineRenderer()//stops the render if not polyline
+    }
     //Detects Tap (Beggining) TO ADD: check if touching "click Sprite"
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first!
@@ -61,13 +77,16 @@ class ViewController: UIViewController {
             let coords = mapView.convert(tapLocation,toCoordinateFrom: mapView)
             print("Tapped at lat: \(coords.latitude) long: \(coords.longitude)")
             makeAnnotation(givenTitle: "Start", pointCoords: coords)
+            locations.append(coords)
             phase = 1
         } else if (phase == 1){ //if set start
             let coords = mapView.convert(tapLocation,toCoordinateFrom: mapView)
             print("Tapped at lat: \(coords.latitude) long: \(coords.longitude)")
             makeAnnotation(givenTitle: "End", pointCoords: coords)
+            locations.append(coords)
             phase = 2
             //create polyline here
+            makePolyline(locations: locations)
         } else { //add turns here + redraw polyline
             
         }
@@ -76,5 +95,6 @@ class ViewController: UIViewController {
     @IBAction func resetTapped(_ sender: Any) {
         removeAnnotations()
         phase = 0
+        locations = []
     }
 }
