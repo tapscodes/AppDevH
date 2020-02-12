e0882847010215d621157cdee688052600c474c7
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2020-02-12 09:09:21 -0500

    Rearranged some code and made the physics body with code

--- App Dev Projects/GyroGamePS/GyroGamePS/GameScene.swift
+++ App Dev Projects/GyroGamePS/GyroGamePS/GameScene.swift
@@ -18,11 +18,26 @@ class GameScene: SKScene {
         //Sets up nodes
         self.scoreLbl = self.childNode(withName: "scoreLbl") as? SKLabelNode
         self.player = self.childNode(withName: "playerSprite") as? SKSpriteNode
+        setupPlayer()
         //Sets up other stuff
         setupGyro()
-        moveObject(x: 0, y: 0)
+        moveObject(x: 0)
     }
-    //MARK: setup functions
+    //MARK: setup/generation functions
+    //Sets up player node
+    func setupPlayer(){
+        player?.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 25, height: 25))
+        player?.physicsBody?.isDynamic = true
+        player?.physicsBody?.affectedByGravity = true
+        player?.physicsBody?.allowsRotation = false
+        player?.physicsBody?.angularDamping = 0
+        player?.physicsBody?.linearDamping = 0
+        player?.physicsBody?.friction = 0
+        player?.physicsBody?.restitution = 0
+        player?.physicsBody?.mass = 1
+        player?.zRotation = 0
+        player?.zPosition = 1
+    }
     //Sets up gyroscope sensor data
     func setupGyro(){
         motionManager.gyroUpdateInterval = 0.2
@@ -34,14 +49,21 @@ class GameScene: SKScene {
                 let z = trueData.rotationRate.z
                 //do motion here
                 print("X: \(x), Y: \(y), Z: \(z)")
-                self.moveObject(x: x, y: y)
+                self.moveObject(x: x)
             }
         }
     }
-    //Moves the object
-    func moveObject(x: Double, y: Double){
-        print("X: \(x), Y: \(y)")
+    //Makes a wall with one piece missing
+    func generateWall(){
+        
     }
+    //MARK: physics functions
+    //Moves the object
+    func moveObject(x: Double){
+        player?.physicsBody?.velocity = CGVector(dx: x, dy: Double((player?.physicsBody?.velocity.dy)!))
+        print("X: \(x)")
+    }
+    //MARK: update function
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
