efd48c9bdc0265541911e78109362dc4710dd68a
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2020-02-12 08:52:13 -0500

    Added Gyroscope Sensor

--- App Dev Projects/GyroGamePS/GyroGamePS/GameScene.swift
+++ App Dev Projects/GyroGamePS/GyroGamePS/GameScene.swift
@@ -8,14 +8,31 @@
 
 import SpriteKit
 import GameplayKit
-
+import CoreMotion
 class GameScene: SKScene {
     //MARK: variables
+    var motionManager : CMMotionManager = CMMotionManager()
     private var scoreLbl : SKLabelNode?
     //MARK: functions
     override func didMove(to view: SKView) {
-        // Get label node from scene and store it for use later
+        //Sets up nodes
         self.scoreLbl = self.childNode(withName: "scoreLbl") as? SKLabelNode
+        //Sets up other stuff
+        setupGyro()
+    }
+    //MARK: setup functions
+    func setupGyro(){
+        motionManager.gyroUpdateInterval = 0.2
+        motionManager.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
+            print(data as Any)
+            if let trueData = data {
+                let x = trueData.rotationRate.x
+                let y = trueData.rotationRate.y
+                let z = trueData.rotationRate.z
+                //do motion here
+                print("X: \(x), Y: \(y), Z: \(z)")
+            }
+        }
     }
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
