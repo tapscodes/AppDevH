0a09780e4c3a784251c8a5b49257686be5583a37
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2020-02-12 10:07:35 -0500

    Have the "walls" constantly moving up, despawning, and respawning

--- App Dev Projects/GyroGamePS/GyroGamePS/GameScene.swift
+++ App Dev Projects/GyroGamePS/GyroGamePS/GameScene.swift
@@ -14,6 +14,7 @@ class GameScene: SKScene {
     private var spawnedWalls: [[SKSpriteNode]] = []
     private var scoreLbl : SKLabelNode?
     private var player: SKSpriteNode?
+    private var time: Double = 0
     //MARK: functions
     override func didMove(to view: SKView) {
         //Sets up nodes
@@ -29,28 +30,34 @@ class GameScene: SKScene {
     //MARK: setup/generation functions
     //Sets up player node
     func setupPlayer(){
-        player?.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        player?.size = CGSize(width: 50, height: 50)
-        player?.position = CGPoint(x: 500, y: 0)
-        setDefaults(sprite: player!, mass: 1, zPosition: 2, affectedByGravity: true, isDyamic: true)
+        player?.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 25, height: 25))
+        player?.size = CGSize(width: 25, height: 25)
+        player?.position = CGPoint(x: 0, y: 0)
+        setDefaults(sprite: player!, mass: 0.000000000000000000000001, zPosition: 2, affectedByGravity: true, isDyamic: true)
+        player?.physicsBody?.restitution = 0.5
     }
     //Makes a wall
     func generateWall(width: CGFloat, height: CGFloat, position: CGPoint) -> SKSpriteNode{
         let wall = SKSpriteNode(color: .red, size: CGSize(width: width, height: height))
         wall.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: width, height: height))
         wall.position = position
-        setDefaults(sprite: wall, mass: 1000, zPosition: 1, affectedByGravity: false, isDyamic: true)
+        setDefaults(sprite: wall, mass: 100000, zPosition: 1, affectedByGravity: false, isDyamic: true)
         wall.physicsBody?.velocity = CGVector(dx: 0, dy: 100)
         return wall
     }
     //Makes a entire wall with one piece missing
     func nextWall(){
         let width: CGFloat = 100
-        let height: CGFloat = 50
-        let wall1: SKSpriteNode = generateWall(width: width, height: height, position: CGPoint(x: 0, y: -100))
-        let wall2: SKSpriteNode = generateWall(width: width, height: height, position: CGPoint(x: -(width * 1.25), y: -100))
-        let wall3: SKSpriteNode = generateWall(width: width, height: height, position: CGPoint(x: (width * 1.25), y: -100))
-        var fullWall: [SKSpriteNode] = [wall1, wall2, wall3]
+        let height: CGFloat = 25
+        let wall1: SKSpriteNode = generateWall(width: width, height: height, position: CGPoint(x: -width / 2, y: -650))
+        let wall2: SKSpriteNode = generateWall(width: width, height: height, position: CGPoint(x: width / 2, y: -650))
+        let wall3: SKSpriteNode = generateWall(width: width, height: height, position: CGPoint(x: -(width * 1.5), y: -650))
+        let wall4: SKSpriteNode = generateWall(width: width, height: height, position: CGPoint(x: (width * 1.5), y: -650))
+        let wall5: SKSpriteNode = generateWall(width: width, height: height, position: CGPoint(x: -(width * 2.5), y: -650))
+        let wall6: SKSpriteNode = generateWall(width: width, height: height, position: CGPoint(x: (width * 2.5), y: -650))
+        let wall7: SKSpriteNode = generateWall(width: width, height: height, position: CGPoint(x: -(width * 3.5), y: -650))
+        let wall8: SKSpriteNode = generateWall(width: width, height: height, position: CGPoint(x: (width * 3.5), y: -650))
+        var fullWall: [SKSpriteNode] = [wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8]
         fullWall.remove(at: Int.random(in: 0...fullWall.count - 1)) //removes a random wall
         for wall in fullWall{ //adds wall to scene
             self.addChild(wall)
@@ -103,8 +110,16 @@ class GameScene: SKScene {
     //MARK: update function
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        if(spawnedWalls.count > 0 && spawnedWalls[0][0].position.y > 200){ //removes wall when it hits a certain point near the top
-            spawnedWalls.removeFirst()
+        self.time += (1/60)
+        if(time > 1.5){ //spawns wall every 2 seconds (based on FPS)
+            nextWall()
+            time = 0
+        }
+        if(spawnedWalls.count > 0 && spawnedWalls[0][0].position.y > 500){ //despawns first wall when it goes over 300
+            for wall in spawnedWalls[0]{
+                wall.removeFromParent()
+            }
+            spawnedWalls.remove(at: 0)
         }
     }
 }
