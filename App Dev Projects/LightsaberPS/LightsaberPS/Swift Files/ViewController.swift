e88d29f11ebe716ffa46c37388bf2d726d68e936
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2020-02-24 09:02:05 -0500

    Changed sensitivity of lighsaber/gyroscope

--- App Dev Projects/LightsaberPS/LightsaberPS/Swift Files/ViewController.swift
+++ App Dev Projects/LightsaberPS/LightsaberPS/Swift Files/ViewController.swift
@@ -1,10 +1,10 @@
-//
-//  ViewController.swift
-//  LightsaberPS
-//
-//  Created by Tristan Pudell-Spatscheck on 2/24/20.
-//  Copyright © 2020 Tristan Pudell-Spatscheck. All rights reserved.
-//
+/*
+  ViewController.swift
+  LightsaberPS
+  Created by Tristan Pudell-Spatscheck on 2/24/20.
+  Copyright © 2020 Tristan Pudell-Spatscheck. All rights reserved.
+Note: Used Gyroscope instead of accelerometer as it measues the current speed (aka hard swings) a lot better.
+ */
 import Foundation
 import UIKit
 import AVFoundation
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         lightsaberBtn.setImage(UIImage(named: "lightsaberOff"), for: .normal)
+        setupGyro()
     }
     //plays a given song on repeat
     func playSound(sound: String){
@@ -30,7 +31,7 @@ class ViewController: UIViewController {
             } catch {
                 print("NO SONG FILE for \(sound)")
             }
-            musicPlayer.numberOfLoops = 1
+            musicPlayer.numberOfLoops = 0
             musicPlayer.play()
     }
     //Sets up gyroscope sensor data
@@ -42,29 +43,28 @@ class ViewController: UIViewController {
                 let x = trueData.rotationRate.x
                 let y = trueData.rotationRate.y
                 let z = trueData.rotationRate.z
-                //plays sound if swing is detected unless indle in which case sabercrash plays
-                if(x > 0.5){
-                    self.playSound(sound: "saberSwing")
-                } else if (y > 0.5) {
-                    self.playSound(sound: "saberSwing")
-                } else if (z > 0.5) {
-                    self.playSound(sound: "saberSwing")
-                } else {
-                    self.playSound(sound: "saberCrash")
+                print("X: \(x), Y: \(y), Z:\(z)")
+                //plays sound if swing is detected unless indle in which case sabercrash plays (3 seems to be threshhold for movement)
+                if(saberOn){
+                    if(x > 5 || y > 5 || z > 5){
+                        self.playSound(sound: "saberSwing")
+                    } else if (!self.musicPlayer.isPlaying){ //plays 'saberCrash' if nothing else is playing
+                        self.playSound(sound: "saberCrash")
+                    }
                 }
             }
         }
     }
     //toggles lightsaber on or off
     @IBAction func lightsaberToggle(_ sender: Any) {
-        if(saberOn){
-            lightsaberBtn.setImage(UIImage(named: "lightsaberOff"), for: .normal)
-            playSound(sound: "saberOff")
-            saberOn = false
-        } else {
+        if((UIDevice.current.orientation == .portrait || UIDevice.current.orientation == .portraitUpsideDown) && !saberOn){ //checks that device is in portrait mode when turned on
             lightsaberBtn.setImage(UIImage(named: "lightsaberOn"), for: .normal)
             playSound(sound: "saberOn")
             saberOn = true
+        } else if (saberOn){
+            lightsaberBtn.setImage(UIImage(named: "lightsaberOff"), for: .normal)
+            playSound(sound: "saberOff")
+            saberOn = false
         }
     }
 }
